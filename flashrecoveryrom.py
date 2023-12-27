6d09ee23a8339385e2f069d7d201e44f32457157
offici5l <127665988+offici5l@users.noreply.github.com>
2023-12-27 08:51:09 +0100

    Update flashrecoveryrom.py

--- flashrecoveryrom.py
+++ flashrecoveryrom.py
@@ -12,8 +12,18 @@ input("Please make sure to place the ROM file in the {} folder .. Then press Ent
 rom_files = [f for f in os.listdir(ROM_FOLDER) if f.endswith(".zip")]
 
 if rom_files:
-    input("Make sure your device is connected in sideload mode. Connect your device using OTG, then press Enter when ready.")
-    os.system("adb sideload {}/{}".format(ROM_FOLDER, rom_files[0]))
+    input("\nMake sure your device is in sideload mode. Connect your device using OTG, then press Enter when ready\n")
 else:
     print("Could not find rom.zip file.")
     exit(1)
+
+while True:
+    status = os.popen("adb get-state").read().strip()
+    print(status)
+    if status == "sideload":
+        break
+    else:
+        input("\nplease Verify that device is in sideload mode ! If so, check that it is connected via otg ! then press Enter\n")
+        continue
+
+os.system("adb sideload {}/{}".format(ROM_FOLDER, rom_files[0]))
