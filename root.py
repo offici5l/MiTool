2c5a826ce6574f8ba983f7d0b8570530895905e3
offici5l <127665988+offici5l@users.noreply.github.com>
2024-01-05 05:43:49 +0100

    Update root.py
    delete sidelaod
    flash with fastboot

--- root.py
+++ root.py
@@ -1,56 +1,37 @@
 import os
 
-download_path = "/sdcard/Download/rmagisk"
+print("\nFlashing root with fastboot mode...\n")
 
-os.makedirs(download_path, exist_ok=True)
+R_FOLDER = "/sdcard/Download/mi-flash-root"
 
-has_magisk = input("\nDo you want to download Magisk? (y/n): ")
+if not os.path.exists(R_FOLDER):
+    os.makedirs(R_FOLDER)
 
-if has_magisk == "y":
-    versions = os.popen("curl -s https://api.github.com/repos/topjohnwu/Magisk/releases | grep 'tag_name' | cut -d '\"' -f 4 | grep -v 'manager-'").read().strip().split("\n")
-    print("Available versions:")
-    print("\n".join(versions))
-    selected_version = input("\nPlease choose the version you want to download: ")
-    os.system(f"curl -L -o {download_path}/Magisk-{selected_version}.zip https://github.com/topjohnwu/Magisk/releases/download/{selected_version}/Magisk-{selected_version}.apk")
-    print(f"Version {selected_version} downloaded successfully to {download_path}")
-
-elif has_magisk == "n":
-    input(f"\nMake sure to place Magisk file in {download_path}. Then press Enter")
-    while True:
-        found_apk = False
-        found_zip = False
-        for file in os.listdir(download_path):
-            if file.endswith(".apk"):
-                nfile = os.path.join(download_path, f"{os.path.splitext(file)[0]}.zip")
-                os.rename(os.path.join(download_path, file), nfile)
-                print(f"\nFile {file} converted to {nfile} \n")
-                found_apk = True
-                break
-            elif file.endswith(".zip"):
-                found_zip = True
-                break
-
-        if found_apk:
-            break
-        elif found_zip:
-            print(f"\nMagisk file in {download_path} is already in the correct format...\n")
-            break
-        else:
-            input(f"\nPlease make sure to place Magisk file in {download_path} folder. Then press Enter\n")
-
-else:
-    print("\nInvalid choice")
-    exit(1)
-
-input("\nMake sure your device is in sideload mode. Connect your device using OTG, then press Enter when ready\n")
+input("\nRequirements: boot.img ,Bootloader unlocked\n\n"
+      "1. Download and install Magisk app\n"
+      "2. Open Magisk app and press the Install button in the Magisk card\n"
+      "3. Choose 'Select and Patch a File' in method, and select the boot.img\n"
+      f"4. Copy magisk_patched-*.img to {R_FOLDER}\n"
+      "5. If you complete the steps, press Enter\n")
 
 while True:
-    status = os.popen("adb get-state").read().strip()
-    print(status)
-    if status == "sideload":
+    img_files = [file for file in os.listdir(R_FOLDER) if file.endswith(".img")]
+
+    if img_files:
+        print(f"\nFile found: {img_files[0]}")
         break
     else:
-        input("\nPlease verify that the device is in sideload mode! If so, check that it is connected via OTG! Then press Enter\n")
+        user_input = input(f"\nNo .img file found in {R_FOLDER}. Please check for the file and press Enter\n")
         continue
 
-os.system(f"adb sideload {download_path}/*.zip")
+input("\nMake sure your device is in fastboot mode. Connect your device using OTG, then press Enter when ready\n")
+
+while True:
+    status = os.popen("fastboot devices | grep -o fastboot").read().strip()
+    if status == "fastboot":
+        break
+    else:
+        input("\nplease Verify that device is in fastboot mode ! If so, check that it is connected via otg ! then press Enter\n")
+        continue
+
+os.system(f"fastboot flash boot {R_FOLDER}/{img_files[0]}")
