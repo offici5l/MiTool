b60a0272aa7193896d21fb6841f5a181372062ef
offici5l <127665988+offici5l@users.noreply.github.com>
2024-01-07 11:04:29 +0100

    Update flashfastbootrom.py

--- flashfastbootrom.py
+++ flashfastbootrom.py
@@ -1,72 +1,109 @@
 import os
+import sys
 
-print("Flashing Fastboot ROM...")
+text = "\n\033[92mMake sure your device is connected in fastboot mode. Connect your device using OTG, then press Enter when ready.\033[0m\n"
 
-ROM_FOLDER = "/sdcard/Download/mi-flash-fastboot-rom"
-
-if not os.path.exists(ROM_FOLDER):
-    os.makedirs(ROM_FOLDER)
-
-input("Please make sure to place the ROM file in the {} folder .. Then press Enter".format(ROM_FOLDER))
-
-if any(file.endswith((".sh", ".lock.sh")) for file in os.listdir(ROM_FOLDER)):
-    flashOption = int(input("Choose an option:\n1. Flash ROM without locking bootloader\n2. Flash ROM with bootloader lock\nEnter the option number: "))
-
-    if flashOption == 1:
-        flashAllScript = next((os.path.join(ROM_FOLDER, file) for file in os.listdir(ROM_FOLDER) if file == "flash_all.sh"), None)
-
-        if flashAllScript:
-            input("Make sure your device is connected in fastboot mode. Connect your device using OTG, then press Enter when ready.")
-            os.system(f"sh {flashAllScript}")
+def check_fastboot_mode():
+    while True:
+        status = os.popen("fastboot devices | grep -o 'fastboot'").read().strip()
+        if status == "fastboot":
+            break
         else:
-            print("Could not find flash_all.sh file.")
-            exit(1)
-    elif flashOption == 2:
-        flashAllLockScript = next((os.path.join(ROM_FOLDER, file) for file in os.listdir(ROM_FOLDER) if file == "flash_all_lock.sh"), None)
+            input("\nVerify that the device is in fastboot mode! If so, check that it is connected via OTG! Then press Enter\n")
+            continue
 
-        if flashAllLockScript:
-            input("Make sure your device is connected in fastboot mode. Connect your device using OTG, then press Enter when ready.")
-            os.system(f"sh {flashAllLockScript}")
-        else:
-            print("Could not find flash_all_lock.sh file.")
-            exit(1)
-    else:
-        print("Invalid option")
-        exit(1)
-else:
-    tgzFile = next((os.path.join(ROM_FOLDER, file) for file in os.listdir(ROM_FOLDER) if file.endswith(".tgz")), None)
 
-    if tgzFile:
-        print("ROM file detected in mi-flash-fastboot-rom folder.")
-        print("The ROM file is being decompressed, please wait")
-        os.system(f"pv {tgzFile} | tar --strip-components=1 -xzf- -C {ROM_FOLDER}/ --wildcards --no-anchored 'flash_all.sh' 'flash_all_lock.sh' 'images'")
-    else:
-        print("No .tgz file detected, assuming ROM files are already uncompressed.")
-
-    if any(file.endswith((".sh", ".lock.sh")) for file in os.listdir(ROM_FOLDER)):
-        flashOption = int(input("Choose an option:\n1. Flash ROM without locking bootloader\n2. Flash ROM with bootloader lock\nEnter the option number: "))
+def flash_selected_result(selected_result):
+    if any(file.endswith(("flash_all.sh", "flash_all_lock.sh")) for file in os.listdir(selected_result)):
+        flashOption = int(input("\nChoose an option:\n\n\033[92m1 -\033[0m Flash without locking bootloader\n\033[92m2 -\033[0m Flash with lock bootloader\n\nEnter the option number: "))
 
         if flashOption == 1:
-            flashAllScript = next((os.path.join(ROM_FOLDER, file) for file in os.listdir(ROM_FOLDER) if file == "flash_all.sh"), None)
-
-            if flashAllScript:
-                input("Make sure your device is connected in fastboot mode. Connect your device using OTG, then press Enter when ready.")
-                os.system(f"sh {flashAllScript}")
-            else:
-                print("Could not find flash_all.sh file.")
-                exit(1)
+            input(text)
+            check_fastboot_mode()
+            flash = "flash_all.sh"
+            os.system(f"sh {selected_result}/{flash}")
         elif flashOption == 2:
-            flashAllLockScript = next((os.path.join(ROM_FOLDER, file) for file in os.listdir(ROM_FOLDER) if file == "flash_all_lock.sh"), None)
-
-            if flashAllLockScript:
-                input("Make sure your device is connected in fastboot mode. Connect your device using OTG, then press Enter when ready.")
-                os.system(f"sh {flashAllLockScript}")
-            else:
-                print("Could not find flash_all_lock.sh file.")
-                exit(1)
+            input(text)
+            check_fastboot_mode()
+            flash_lock = "flash_all_lock.sh"
+            os.system(f"sh {selected_result}/{flash_lock}")
         else:
-            print("Invalid option")
+            print("\nInvalid option\n")
             exit(1)
     else:
-        print("No Rom file detected in mi-flash-fastboot-rom folder. Please make sure to place the ROM file in the mi-flash-fastboot-rom folder.")
+        print("\ninvalid directory 'exit'\n")
         exit(1)
+
+def decompress_and_flash_rom(tgz_file_name):
+    ROM_FOLDER = "/sdcard/Download/mi-flash-fastboot-rom"
+    
+    if not os.path.exists(ROM_FOLDER):
+        os.makedirs(ROM_FOLDER)
+
+    print(f"\n\033[92mdecompressed..., please wait\033[0m\n")
+    
+    os.system(f"pv -bpe {tgz_file_name} | tar --strip-components=1 -xzf- -C {ROM_FOLDER}/ --wildcards --no-anchored 'flash_all.sh' 'flash_all_lock.sh' 'images' 2>/dev/null")
+    
+    if any(file.endswith(("flash_all.sh", "flash_all_lock.sh")) for file in os.listdir(ROM_FOLDER)):
+        flashOption = int(input("\nChoose an option:\n\n\033[92m1 -\033[0m Flash without locking bootloader\n\033[92m2 -\033[0m Flash with lock bootloader\n\nEnter the option number: "))
+
+        if flashOption == 1:
+            input(text)
+            check_fastboot_mode()
+            flash = "flash_all.sh"
+            os.system(f"sh {ROM_FOLDER}/{flash}")
+        elif flashOption == 2:
+            input(text)
+            check_fastboot_mode()
+            flash_lock = "flash_all_lock.sh"
+            os.system(f"sh {ROM_FOLDER}/{flash_lock}")
+        else:
+            print("\nInvalid option\n")
+            exit(1)
+    else:
+        print("\ninvalid tgz 'exit'\n")
+        exit(1)
+
+
+while True:
+    if len(sys.argv) < 2:
+        target_name = input("\n\033[92mEnter target name\033[0m: ")
+    else:
+        target_name = sys.argv[1]
+
+    if target_name:
+        break
+
+result_paths = []
+for root, dirs, files in os.walk("/sdcard"):
+    if target_name in files or target_name in dirs:
+        result_paths.append(os.path.join(root, target_name))
+
+if result_paths:
+    for i, result in enumerate(result_paths, start=1):
+        print(f"\n \033[92m{i}\033[0m - {result}\n")
+
+    while True:
+        try:
+            selected_index = int(input(f"\nEnter correct \033[92mnumber\033[0m for confirmation: "))
+            if 1 <= selected_index <= len(result_paths):
+                break
+            else:
+                print("\nInvalid selection !")
+        except ValueError:
+            print("\nInvalid input !")
+
+    selected_result = result_paths[selected_index - 1]
+
+    if selected_result.endswith(".tgz"):
+        decompress_and_flash_rom(selected_result)        
+    elif os.path.isdir(selected_result):
+        flash_selected_result(selected_result)
+    elif os.path.isfile(selected_result):
+        print(f"\n'{selected_result}' is not .tgz file\n")
+        exit()
+    else:
+        print(f"\nUnable to determine the type of {selected_result} \n")
+else:
+    print(f"Result '{target_name}' not found")
+    exit()
