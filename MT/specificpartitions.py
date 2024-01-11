a2ba47293fe3dc3f7a7c30d502085a4756f52fbb
offici5l <127665988+offici5l@users.noreply.github.com>
2024-01-11 13:11:08 +0100

    Update specificpartitions.py

--- MT/specificpartitions.py
+++ MT/specificpartitions.py
@@ -1,117 +1,46 @@
 import os
 import sys
 
-while True:
-    if len(sys.argv) < 2:
-        target_filename = input("\n\033[92mEnter target name\033[0m: ")
-    else:
-        target_filename = sys.argv[1]
-
-    if target_filename:
-        break 
-
-file_paths = []
-for root, dirs, files in os.walk("/sdcard"):
-    if target_filename in files:
-        file_paths.append(os.path.join(root, target_filename))
-
-if file_paths:
-    for i, file in enumerate(file_paths, start=1):
-        print(f"\n \033[92m{i}\033[0m - {file}\n")
-
-    while True:
-        try:
-            selected_index = int(input(f"\nEnter correct \033[92mnumber\033[0m for confirmation: "))
-            if 1 <= selected_index <= len(file_paths):
-                break
-            else:
-                print("\nInvalid selection !")
-        except ValueError:
-            print("\nInvalid input !")
-
-    selected_file = file_paths[selected_index - 1]
-    print(f"\nSelected file '{selected_file}'\n")
+if len(sys.argv) != 2:
+    flash_type = input("\nEnter type img you want to flash (recovery/boot/vbmeta/vbmeta_system/metadata/metadata/dtbo/cust/super/userdata): ").lower()
+    if flash_type not in ["recovery", "boot", "vbmeta", "vbmeta_system", "metadata", "dtbo", "cust", "super", "userdata"]:
+        print("\nInvalid input!\n")
+        sys.exit(1)
 else:
-    print(f"file {file_paths} not found")
-    exit()
+    flash_type = sys.argv[1].lower()
+    if flash_type not in ["recovery", "boot", "vbmeta", "vbmeta_system", "metadata", "dtbo", "cust", "super", "userdata"]:
+        print("Invalid image type!")
+        sys.exit(1)
 
-input("\nMake sure your device is in fastboot mode. Connect your device using OTG, then press Enter when ready\n")
 
-while True:
-    status = os.popen("fastboot devices | grep -o 'fastboot'").read().strip()
-    if status == "fastboot":
-        break
-    else:
-        input("\nVerify that the device is in fastboot mode! If so, check that it is connected via OTG! Then press Enter\n")
+result_paths = []
+
+for root, dirs, files in os.walk("/sdcard"):
+    if "Android" in root:
         continue
 
-if os.path.isfile(selected_file):
-    print(f"\nflashing {selected_file} ...\n")
+    img_files = [f for f in files if f.endswith(".img")]
+    result_paths.extend([os.path.join(root, f) for f in img_files])
 
+if not result_paths:
+    print("No '.img' files found.")
+    exit()
 
-    if selected_file.endswith("crclist.txt"):
-        os.system(f"fastboot flash crclist {selected_file}")
-    elif selected_file.endswith("sparsecrclist.txt"):
-        os.system(f"fastboot flash sparsecrclist {selected_file}")
-    elif selected_file.endswith("xbl.elf"):
-        os.system(f"fastboot flash xbl {selected_file}")
-    elif selected_file.endswith("xbl_config.elf"):
-        os.system(f"fastboot flash xbl_config {selected_file}")
-    elif selected_file.endswith("rpm.mbn"):
-        os.system(f"fastboot flash rpm {selected_file}")
-    elif selected_file.endswith("tz.mbn"):
-        os.system(f"fastboot flash tz {selected_file}")
-    elif selected_file.endswith("hyp.mbn"):
-        os.system(f"fastboot flash hyp {selected_file}")
-    elif selected_file.endswith("featenabler.mbn"):
-        os.system(f"fastboot flash featenabler {selected_file}")
-    elif selected_file.endswith("NON-HLOS.bin"):
-        os.system(f"fastboot flash modem {selected_file}")
-    elif selected_file.endswith("BTFM.bin"):
-        os.system(f"fastboot flash bluetooth {selected_file}")
-    elif selected_file.endswith("abl.elf"):
-        os.system(f"fastboot flash abl {selected_file}")
-    elif selected_file.endswith("dspso.bin"):
-        os.system(f"fastboot flash dsp {selected_file}")
-    elif selected_file.endswith("km4.mbn"):
-        os.system(f"fastboot flash keymaster {selected_file}")
-    elif selected_file.endswith("boot.img"):
-        os.system(f"fastboot flash boot {selected_file}")
-    elif selected_file.endswith("cmnlib.mbn"):
-        os.system(f"fastboot flash cmnlib {selected_file}")
-    elif selected_file.endswith("cmnlib64.mbn"):
-        os.system(f"fastboot flash cmnlib64 {selected_file}")
-    elif selected_file.endswith("devcfg.mbn"):
-        os.system(f"fastboot flash devcfg {selected_file}")
-    elif selected_file.endswith("qupv3fw.elf"):
-        os.system(f"fastboot flash qupfw {selected_file}")
-    elif selected_file.endswith("vbmeta.img"):
-        os.system(f"fastboot flash vbmeta {selected_file}")
-    elif selected_file.endswith("vbmeta_system.img"):
-        os.system(f"fastboot flash vbmeta_system {selected_file}")
-    elif selected_file.endswith("recovery.img"):
-        os.system(f"fastboot flash recovery {selected_file}")
-    elif selected_file.endswith("metadata.img"):
-        os.system(f"fastboot flash metadata {selected_file}")
-    elif selected_file.endswith("dtbo.img"):
-        os.system(f"fastboot flash dtbo {selected_file}")
-    elif selected_file.endswith("imagefv.elf"):
-        os.system(f"fastboot flash imagefv {selected_file}")
-    elif selected_file.endswith("uefi_sec.mbn"):
-        os.system(f"fastboot flash uefisecapp {selected_file}")
-    elif selected_file.endswith("logfs_ufs_8mb.bin"):
-        os.system(f"fastboot flash logfs {selected_file}")
-    elif selected_file.endswith("storsec.mbn"):
-        os.system(f"fastboot flash storsec {selected_file}")
-    elif selected_file.endswith("multi_image.mbn"):
-        os.system(f"fastboot flash multiimgoem {selected_file}")
-    elif selected_file.endswith("cache.img"):
-        os.system(f"fastboot flash cache {selected_file}")
-    elif selected_file.endswith("super.img"):
-        os.system(f"fastboot flash super {selected_file}")
-    elif selected_file.endswith("userdata.img"):
-        os.system(f"fastboot flash userdata {selected_file}")
-    elif selected_file.endswith("cust.img"):
-        os.system(f"fastboot flash cust {selected_file}")
-    else:
-        print("Unknown file type.")
+for i, result in enumerate(result_paths, start=1):
+    print(f"\n \033[92m{i}\033[0m - {result}\n")
+
+while True:
+    try:
+        selected_index = int(input(f"\nType the correct \033[92mnumber\033[0m for {flash_type} you want to flash: "))
+        if 1 <= selected_index <= len(result_paths):
+            break
+        else:
+            print("\nInvalid selection!")
+    except ValueError:
+        print("\nInvalid input!")
+
+selected_result = result_paths[selected_index - 1]
+
+print(f"\nImg selected: \033[92m{selected_result}\033[0m\n\nCheck if the device is connected via OTG! ..\n")
+
+os.system(f"fastboot flash {flash_type} {selected_result}")
