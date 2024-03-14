67e675bb4f4599dbaf34217fe18497550dee4d97
offici5l <127665988+offici5l@users.noreply.github.com>
2024-03-14 01:34:25 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,52 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-yes | pkg update && yes | pkg upgrade && yes | pkg install openssl-tool && yes | pkg install openssl && yes | pkg install gnupg && yes | pkg install coreutils && yes | pkg install wget && yes | pkg install pv && yes | pkg install python3 && yes | pip install requests && yes | pip3 install pyshorteners && yes | pip3 install pycryptodomex && yes | pip install termcolor && mkdir -p $PREFIX/etc/apt/sources.list.d && echo -e "deb https://nohajc.github.io termux extras" > $PREFIX/etc/apt/sources.list.d/termux-adb.list && wget -qP $PREFIX/etc/apt/trusted.gpg.d https://nohajc.github.io/nohajc.gpg && yes | apt update && yes | apt install termux-adb && cp -f $PREFIX/bin/termux-adb $PREFIX/bin/adb && cp -f $PREFIX/bin/termux-fastboot $PREFIX/bin/fastboot
+if [ ! -e "/data/data/com.termux/files/home/.bash_history" ]; then
+    yes | apt update && yes | apt upgrade
+fi
 
-files=("mitool" "flashfastbootromatest.py" "flashfastbootrom.py" "un-lock.py" "bypass-bl.py" "flashwithsideloadmode.py" "mitoolV")
+if ! command -v adb &>/dev/null || ! command -v fastboot &>/dev/null; then
+    echo "Installing ADB and Fastboot..."
+    yes | pkg uninstall termux-adb 2>/dev/null
+    curl -s https://raw.githubusercontent.com/nohajc/termux-adb/master/install.sh | bash
+    ln -s $PREFIX/bin/termux-fastboot $PREFIX/bin/fastboot
+    ln -s $PREFIX/bin/termux-adb $PREFIX/bin/adb
+fi
 
-for file in "${files[@]}"; do
-    echo -e "\033[32mupdate $file...\033[0m"
-    curl "https://raw.githubusercontent.com/offici5l/MiTool/master/MT/$file" -o "$PREFIX/bin/$file" &&
-    chmod +x "$PREFIX/bin/$file"
-done
+if ! command -v python3 &>/dev/null; then
+    echo "Installing Python3..."
+    yes | pkg install python3
+fi
 
-printf "
-use command: \e[1;32mmitool\e[0m\n
-"
+if ! python3 -c "import Cryptodome" &>/dev/null; then
+    echo "Installing Cryptodome..."
+    yes | pip install pycryptodomex
+fi
+
+if ! python3 -c "import urllib3" &>/dev/null; then
+    echo "Installing urllib3..."
+    yes | pip install urllib3
+fi
+
+if ! python3 -c "import requests" &>/dev/null; then
+    echo "Installing requests..."
+    yes | pip install requests
+fi
+
+echo -e "\033[32mupdate mitool...\033[0m"
+curl "https://raw.githubusercontent.com/offici5l/MiTool/master/MT/mitool.py" -o "$PREFIX/bin/mitool" && chmod +x "$PREFIX/bin/mitool"
+
+echo -e "\033[32mupdate flashfastbootrom...\033[0m"
+curl "https://raw.githubusercontent.com/offici5l/MiTool/master/MT/flashfastbootrom.py" -o "$PREFIX/bin/flashf" && chmod +x "$PREFIX/bin/flashf"
+
+echo -e "\033[32mupdate flashwithsideloadmode...\033[0m"
+curl "https://raw.githubusercontent.com/offici5l/MiTool/master/MT/flashwithsideloadmode.py" -o "$PREFIX/bin/flashz" && chmod +x "$PREFIX/bin/flashz"
+
+echo -e "\033[32mupdate MiUnlockTool...\033[0m"
+curl "https://raw.githubusercontent.com/offici5l/MiUnlockTool/master/MiUnlockTool.py" -o "$PREFIX/bin/miunlock" && chmod +x "$PREFIX/bin/miunlock"
+
+echo -e "\033[32mupdate MiBypassTool...\033[0m"
+curl "https://raw.githubusercontent.com/offici5l/MiBypassTool/master/MiBypassTool.py" -o "$PREFIX/bin/mibypass" && chmod +x "$PREFIX/bin/mibypass"
+
+if [ ! -e "$PREFIX/bin/mitool" ]; then
+    printf "use command: \e[1;32mmitool\e[0m\n"
+fi
