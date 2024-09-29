a0954a02e0bb9d11d8e2ed0199737bc1e1377447
offici5l <127665988+offici5l@users.noreply.github.com>
2024-09-29 08:46:14 +0100

    Update mihelp.py

--- MT/mihelp.py
+++ MT/mihelp.py
@@ -2,40 +2,31 @@
 
 RESET="\033[0m"
 GREEN="\033[1;32m"
+GRAY="\033[1;30m"
+LINE=f"{GRAY}\n\n{'*'*56}\n\n{RESET}"
 
-LEAF_GREEN="\033[38;5;34m"
+print(f"\033[H\033[J")
 
-LINE = f"{LEAF_GREEN}{'#'*56}{RESET}\n"
+print(f"""{LINE}{GRAY}To report issues or share feedback, visit:\n- GitHub Issues: github.com/offici5l/MiTool/issues\n- Telegram Group: t.me/Offici5l_Group\n\n{RESET}{GREEN}fastboot{RESET}{GRAY} and{GREEN} adb {GRAY}commands can be used in the terminal.\n\nMiTool is automatically updated when a new version is available, but you can do this manually by: {GREEN}mitool u{RESET}{LINE}{LINE}{LINE}""")
 
-print("\033[H\033[J")
-
-print(f"""
-
-{LINE}
-
-{GREEN}fastboot{RESET} and {GREEN}adb{RESET} commands can be used in the terminal.
-
-{LINE}
-
-MiTool is automatically updated when a new version is available, but you can do this manually by: 
-{GREEN}mitool u{RESET}
-
-{LINE}
+message = f"""
 
 Lock Bootloader:
+
 notice: Prior to initiating the process, ensure that the partitions are clean (If you've previously rooted your device, flash the clean boot.img or if any modifications have been made to any partition, flash the clean partition-name.img) to prevent any potential issues in the future.
 Type: {GREEN}fastboot oem lock{RESET}
 
 {LINE}
 
 Flash Custom Recovery:
+
 Type: {GREEN}fastboot flash recovery /path/name.img{RESET}
 Example:
 fastboot flash recovery /sdcard/download/recovery.img
 
 {LINE}
 
-Flash Root:
+Flash Root(Magisk):
 1. Download and install Magisk app
 2. Open Magisk app, press Install in the Magisk card
 3. Choose 'Select and Patch a File', select boot.img
@@ -52,12 +43,6 @@ Type:
 Example:
 fastboot flash super /sdcard/download/super.img
 
-{LINE}
+"""
 
-To report issues or share feedback, visit:
-- GitHub Issues: github.com/offici5l/MiTool/issues
-- Telegram Group: t.me/Offici5l_Group
-
-{LINE}
-
-""")
+print(message)
